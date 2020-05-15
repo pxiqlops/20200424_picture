8aa5ecf150f6d97547bca57de50232d48dfde4fb
tatsumi mukai <63081975+pxiqlops@users.noreply.github.com>
2020-05-15 17:38:00 +0900

    画像表示プログラム

---
+++ 0424/Form1.Designer.vb
@@ -0,0 +1,142 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.SplitContainer2 = New System.Windows.Forms.SplitContainer()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.lstFileName = New System.Windows.Forms.ListBox()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer2.Panel1.SuspendLayout()
+        Me.SplitContainer2.Panel2.SuspendLayout()
+        Me.SplitContainer2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.SplitContainer2)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.WebBrowser1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(800, 450)
+        Me.SplitContainer1.SplitterDistance = 266
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'SplitContainer2
+        '
+        Me.SplitContainer2.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer2.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer2.Name = "SplitContainer2"
+        Me.SplitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer2.Panel1
+        '
+        Me.SplitContainer2.Panel1.Controls.Add(Me.btnLoad)
+        Me.SplitContainer2.Panel1.Controls.Add(Me.btnSave)
+        '
+        'SplitContainer2.Panel2
+        '
+        Me.SplitContainer2.Panel2.Controls.Add(Me.lstFileName)
+        Me.SplitContainer2.Size = New System.Drawing.Size(262, 446)
+        Me.SplitContainer2.SplitterDistance = 87
+        Me.SplitContainer2.TabIndex = 0
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Location = New System.Drawing.Point(75, 12)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(57, 23)
+        Me.btnLoad.TabIndex = 1
+        Me.btnLoad.Text = "Load"
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(12, 12)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(57, 23)
+        Me.btnSave.TabIndex = 0
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'lstFileName
+        '
+        Me.lstFileName.AllowDrop = True
+        Me.lstFileName.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.lstFileName.FormattingEnabled = True
+        Me.lstFileName.ItemHeight = 15
+        Me.lstFileName.Location = New System.Drawing.Point(0, 0)
+        Me.lstFileName.Name = "lstFileName"
+        Me.lstFileName.Size = New System.Drawing.Size(262, 355)
+        Me.lstFileName.TabIndex = 0
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebBrowser1.Location = New System.Drawing.Point(0, 0)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(526, 446)
+        Me.WebBrowser1.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.SplitContainer2.Panel1.ResumeLayout(False)
+        Me.SplitContainer2.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents SplitContainer2 As SplitContainer
+    Friend WithEvents lstFileName As ListBox
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents btnSave As Button
+End Class
---
+++ 0424/Form1.vb
@@ -0,0 +1,87 @@
+﻿Public Class Form1
+    Dim FullPaths As New ArrayList
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'フォームタイトル表示
+        Me.Text = "画像表示プログラム"
+    End Sub
+    Private Sub lstFileName_DragEnter(ByVal sender As Object, e As DragEventArgs) Handles lstFileName.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        Else
+            e.Effect = DragDropEffects.None
+        End If
+    End Sub
+
+    Private Sub lstFileName_DragDrop(ByVal sender As Object, e As DragEventArgs) Handles lstFileName.DragDrop
+        Dim FileName As String
+
+        FileName = CType(e.Data.GetData(DataFormats.FileDrop), String())(0)
+
+        FullPaths.Add(FileName)
+        lstFileName.Items.Add(IO.Path.GetFileName(FileName))
+
+        btnSave.Enabled = True
+    End Sub
+
+    Private Sub lstFileName_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lstFileName.SelectedIndexChanged
+        Dim FullPath As String
+
+        FullPath = FullPaths(lstFileName.SelectedIndex)
+
+        WebBrowser1.Navigate(FullPath)
+
+        Me.Text = "画像表示プログラム - " & FullPath
+    End Sub
+
+    Private Sub btnSave_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSave.Click
+
+        Dim Dialog As New SaveFileDialog
+
+        If Dialog.ShowDialog(Me) = Windows.Forms.DialogResult.OK Then
+
+            Dim Writer As New IO.StreamWriter(Dialog.FileName)
+
+            For Index As Integer = 0 To FullPaths.Count - 1
+                Writer.WriteLine(FullPaths(Index))
+            Next
+
+            Writer.Close()
+
+        End If
+
+    End Sub
+
+
+    Private Sub btnLoad_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnLoad.Click
+
+        Dim Dialog As New OpenFileDialog
+
+        If Dialog.ShowDialog(Me) = Windows.Forms.DialogResult.OK Then
+
+            Dim Value As String
+            Dim Reader As New IO.StreamReader(Dialog.FileName)
+
+            FullPaths.Clear()
+            lstFileName.Items.Clear()
+
+            Do
+                Value = Reader.ReadLine
+                If Value Is Nothing Then
+                    Exit Do
+                Else
+                    FullPaths.Add(Value)
+                    lstFileName.Items.Add(IO.Path.GetFileName(Value))
+                End If
+            Loop
+
+            Reader.Close()
+
+            btnSave.Enabled = True
+
+        End If
+
+    End Sub
+
+
+
+End Class
